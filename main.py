1b103b8ebd32446edb535143805b2f4137420ffa
Yagiz Boz <yagizboz34@gmail.com>
2026-08-03 14:39:40 +0300

    Refactored  code into functions

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
-signal_type = input("Continuous or discrete? (c/d): ")
-if signal_type == "c":
-    amplitude = float(input("Enter amplitude[V]: "))
-    frequency = float(input("Enter frequency[Hz]: "))
-    sample_number = int(input("Enter sample number[n]: "))
-    t = np.linspace(0, 1 / frequency, int(sample_number))
+
+
+def continuous_signal():
+    amplitude = float(input("Enter amplitude [V]: "))
+    frequency = float(input("Enter frequency [Hz]: "))
+    sample_number = int(input("Enter sample number [n]: "))
+
+    t = np.linspace(0, 1 / frequency, sample_number)
     signal = amplitude * np.sin(2 * np.pi * frequency * t)
 
     plt.plot(t, signal)
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
-    plt.title("Sine Wave")
-    plt.grid()
+    plt.title("Continuous-Time Sine Wave")
+    plt.grid(True)
     plt.show()
 
-elif signal_type == "d":
-    amplitude = float(input("Enter amplitude[V]: "))
-    omega = float(input("Enter angular frequency[rad/sample]: "))
-    sample_number = int(input("Enter sample number[n]: "))
+
+def discrete_signal():
+    amplitude = float(input("Enter amplitude [V]: "))
+    omega = float(input("Enter angular frequency [rad/sample]: "))
+    sample_number = int(input("Enter sample number [n]: "))
 
     n = np.arange(sample_number)
-
     signal = amplitude * np.sin(omega * n)
 
     plt.stem(n, signal)
     plt.xlabel("n")
     plt.ylabel("Amplitude")
     plt.title("Discrete-Time Sine Wave")
-    plt.grid()
+    plt.grid(True)
     plt.show()
-else:
-    print("Invalid signal type!")
 
+
+def main():
+    signal_type = input("Continuous or discrete? (c/d): ").lower()
+
+    if signal_type == "c":
+        continuous_signal()
+
+    elif signal_type == "d":
+        discrete_signal()
+
+    else:
+        print("Invalid signal type!")
+
+
+if __name__ == "__main__":
+    main()
